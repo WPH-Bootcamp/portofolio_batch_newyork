64123ef9d466d4f27fd02fd287d417326bd97d81
Henry Rivardo <henrysihombing20@gmail.com>
2026-06-01 20:41:09 +0700

    button

---
+++ src/app/home/partials/navbar.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Button>Get Started</Button>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
+import Navbar from "./home/partials/navbar";
+
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-display-2xl font-bold">
-        Your Trusted IT{" "}
-        <span className="text-primary-300 ">Partner for Digital </span>
-        Success
-      </h1>
+    <div>
+      <Navbar />
     </div>
   );
 }
