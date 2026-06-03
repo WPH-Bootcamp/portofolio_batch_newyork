9d5378c30af772e3bfd9cdb14670e453b5c50c56
Henry Rivardo <henrysihombing20@gmail.com>
2026-06-03 19:53:54 +0700

    feat: section navbar

--- src/app/home/partials/navbar.tsx
+++ src/app/home/partials/navbar.tsx
@@ -3,6 +3,14 @@ import { navigationData } from "@/constant/navigation-data";
 import Image from "next/image";
 import Link from "next/link";
 
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+
 const Navbar = () => {
   return (
     <header className="fixed top-0 z-50 w-full">
@@ -30,11 +38,42 @@ const Navbar = () => {
         </nav>
 
         {/* button */}
-        <Button asChild className="hidden lg:flex ">
+        <Button asChild className="hidden lg:flex">
           <Link href="#contact">Get Started</Link>
         </Button>
 
         {/* sheet button */}
+        <Sheet>
+          <SheetTrigger asChild>
+            <Menu
+              className="cursor-pointer lg:hidden"
+              aria-label="Open main navigation"
+            />
+          </SheetTrigger>
+          <SheetContent>
+            <nav className="mt-16" aria-label="Mobile Navigation">
+              <ul className="flex flex-col gap-4">
+                {navigationData.map((data) => (
+                  <li key={data.label}>
+                    <SheetClose asChild>
+                      <Link
+                        className="hover:text-primary-200 p-4"
+                        href={data.href}
+                      >
+                        {data.label}
+                      </Link>
+                    </SheetClose>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+            <Button asChild className="mt-3 w-full">
+              <SheetClose asChild>
+                <Link href="#contact">Get Started</Link>
+              </SheetClose>
+            </Button>
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   );
--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -3,12 +3,28 @@
 import { Dialog as SheetPrimitive } from "radix-ui";
 import * as React from "react";
 
-import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { XIcon } from "lucide-react";
+import { useMedia } from "react-use";
 
 function Sheet({ ...props }: React.ComponentProps<typeof SheetPrimitive.Root>) {
-  return <SheetPrimitive.Root data-slot="sheet" {...props} />;
+  const isLargeIsh = useMedia("(min-width:1024px", false);
+  const [open, setOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isLargeIsh) {
+      setOpen(false);
+    }
+  }, [isLargeIsh]);
+
+  return (
+    <SheetPrimitive.Root
+      data-slot="sheet"
+      open={open}
+      onOpenChange={setOpen}
+      {...props}
+    />
+  );
 }
 
 function SheetTrigger({
@@ -62,22 +78,28 @@ function SheetContent({
         data-slot="sheet-content"
         data-side={side}
         className={cn(
-          "fixed z-50 flex flex-col gap-4 bg-popover bg-clip-padding text-sm text-popover-foreground shadow-lg transition duration-200 ease-in-out data-[side=bottom]:inset-x-0 data-[side=bottom]:bottom-0 data-[side=bottom]:h-auto data-[side=bottom]:border-t data-[side=left]:inset-y-0 data-[side=left]:left-0 data-[side=left]:h-full data-[side=left]:w-3/4 data-[side=left]:border-r data-[side=right]:inset-y-0 data-[side=right]:right-0 data-[side=right]:h-full data-[side=right]:w-3/4 data-[side=right]:border-l data-[side=top]:inset-x-0 data-[side=top]:top-0 data-[side=top]:h-auto data-[side=top]:border-b data-[side=left]:sm:max-w-sm data-[side=right]:sm:max-w-sm data-open:animate-in data-open:fade-in-0 data-[side=bottom]:data-open:slide-in-from-bottom-10 data-[side=left]:data-open:slide-in-from-left-10 data-[side=right]:data-open:slide-in-from-right-10 data-[side=top]:data-open:slide-in-from-top-10 data-closed:animate-out data-closed:fade-out-0 data-[side=bottom]:data-closed:slide-out-to-bottom-10 data-[side=left]:data-closed:slide-out-to-left-10 data-[side=right]:data-closed:slide-out-to-right-10 data-[side=top]:data-closed:slide-out-to-top-10",
+          "bg-base-background data-[state=open]:animate-in data-[state=closed]:animate-out fixed z-50 flex flex-col gap-3 px-4 shadow-lg transition ease-in-out data-[state=open]:duration-500 data-[state=closed]:duration-300",
+          side === "right" &&
+            "data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right inset-y-0 right-0 h-full w-full",
+          side === "left" &&
+            "data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left inset-y-0 left-0 h-full w-full",
+          side === "top" &&
+            "data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top inset-x-0 top-0 h-auto border-b",
+          side === "bottom" &&
+            "data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom inset-x-0 bottom-0 h-auto border-t",
           className,
         )}
         {...props}
       >
+        <SheetTitle>Title</SheetTitle>
         {children}
         {showCloseButton && (
-          <SheetPrimitive.Close data-slot="sheet-close" asChild>
-            <Button
-              variant="ghost"
-              className="absolute top-3 right-3"
-              size="icon-sm"
-            >
-              <XIcon />
-              <span className="sr-only">Close</span>
-            </Button>
+          <SheetPrimitive.Close
+            className="top-4 right-4 absolute cursor-pointer rounded-xs"
+            data-slot="sheet-close"
+            asChild
+          >
+            <XIcon className="size-6" color="#fdfdfd" />
           </SheetPrimitive.Close>
         )}
       </SheetPrimitive.Content>
@@ -113,7 +135,7 @@ function SheetTitle({
     <SheetPrimitive.Title
       data-slot="sheet-title"
       className={cn(
-        "font-heading text-base font-medium text-foreground",
+        "font-heading text-base font-semibold hidden text-foreground",
         className,
       )}
       {...props}
@@ -135,6 +157,12 @@ function SheetDescription({
 }
 
 export {
-  Sheet, SheetClose,
-  SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
 };
